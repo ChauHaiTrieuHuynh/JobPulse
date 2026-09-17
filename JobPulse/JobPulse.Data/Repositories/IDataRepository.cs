25e26fcbb275e3dfba0256001f20ee838a9ee441
Chau Hai Trieu Huynh <97211030+ChauHaiTrieuHuynh@users.noreply.github.com>
2026-09-17 12:27:54 -0400

    Add: configuration of automapper and update the architecture preference

--- JobPulse/JobPulse.Data/Repositories/IDataRepository.cs
+++ JobPulse/JobPulse.Data/Repositories/IDataRepository.cs
@@ -1,9 +1,8 @@
-﻿using JobPulse.Service.DTOs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace JobPulse.Service.Services
+namespace JobPulse.Data.Repositories
 {
     public interface IDataRepository<T>
     {
---
+++ JobPulse/JobPulse.Service/Services/interface/IJobPulseRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobPulse.Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JobPulse.Service.Services
+{
+    public interface IJobPulseRepository
+    {
+        Task<IEnumerable<JobPostingDto>> GetJobPostingClient();
+
+    }
+}
