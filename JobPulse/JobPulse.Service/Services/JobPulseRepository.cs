0d011329764a3bb2e5774b404f8fe925b473851c
Chau Hai Trieu Huynh <97211030+ChauHaiTrieuHuynh@users.noreply.github.com>
2026-09-17 19:58:35 -0400

    Temp: Temporary push code to pull on another device

--- JobPulse/JobPulse.Service/Services/JobPulseRepository.cs
+++ JobPulse/JobPulse.Service/Services/JobPulseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using JobPulse.Service.DTOs;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +8,27 @@ namespace JobPulse.Service.Services
 {
     public class JobPulseRepository : IJobPulseRepository
     {
+        private readonly IJobSource _jobSource;
+        private readonly ILogger _logger;
+
+        public JobPulseRepository(IJobSource jobSource, ILogger logger)
+        {
+            _jobSource = jobSource;
+            _logger = logger;
+        }
         public Task<IEnumerable<JobPostingDto>> GetJobPostingClient()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return null;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get Job Source ");
+                //sending warning email later
+                //To Do List
+                throw;
+            }
         }
     }
 }
